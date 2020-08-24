d2b05ccf4a14361493f0e4ae736be999c95c74f1
ovinazarov <ovinazarov@greenatom.ru>
2020-08-24 14:44:54 +0300

    Updates

---
+++ precache-manifest.d3e64144e02cea37fb82ae170bb49b80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324f578aaf933259ebca7b80309c2a57",
+    "url": "/SimpleWeatherApp/index.html"
+  },
+  {
+    "revision": "123bd70fa035895d6193",
+    "url": "/SimpleWeatherApp/static/css/3.c27a359e.chunk.css"
+  },
+  {
+    "revision": "bc05db12cfcdad4401f4",
+    "url": "/SimpleWeatherApp/static/css/4.07edd988.chunk.css"
+  },
+  {
+    "revision": "3130fcd4ffc2f7b86c4b",
+    "url": "/SimpleWeatherApp/static/css/main.6e3c36a5.chunk.css"
+  },
+  {
+    "revision": "e294714e5bf98c5d242c",
+    "url": "/SimpleWeatherApp/static/js/2.321a727c.chunk.js"
+  },
+  {
+    "revision": "123bd70fa035895d6193",
+    "url": "/SimpleWeatherApp/static/js/3.717c4860.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/SimpleWeatherApp/static/js/3.717c4860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc05db12cfcdad4401f4",
+    "url": "/SimpleWeatherApp/static/js/4.2896a1ec.chunk.js"
+  },
+  {
+    "revision": "3130fcd4ffc2f7b86c4b",
+    "url": "/SimpleWeatherApp/static/js/main.322e5115.chunk.js"
+  },
+  {
+    "revision": "9a5cd8e6153ccacd1308",
+    "url": "/SimpleWeatherApp/static/js/runtime-main.9d0be1ab.js"
+  }
+]);
